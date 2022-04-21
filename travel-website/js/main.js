4f5e01f5dce6bf5caf0c2df5e06004a54c410b40
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-04-21 14:19:46 +0300

    Travel website: implemented hamburger menu for mobile devices

--- travel-website/js/main.js
+++ travel-website/js/main.js
@@ -5,6 +5,9 @@ const loginBtn = document.querySelector("#login-btn");
 const loginForm = document.querySelector(".login-form-container");
 const loginClose = document.querySelector("#form-close");
 
+const menuBar = document.querySelector("#menu-bar");
+const navbar = document.querySelector("nav.navbar");
+
 searchBtn.addEventListener("click", (e) => {
   searchBtn.classList.toggle("fa-times");
   searchBar.classList.toggle("active");
@@ -14,6 +17,11 @@ loginBtn.addEventListener("click", (e) => {
   loginForm.classList.add("active");
 });
 
+menuBar.addEventListener("click", (e) => {
+  menuBar.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
+
 loginClose.addEventListener("click", (e) => {
   loginForm.classList.remove("active");
 });
@@ -21,4 +29,7 @@ loginClose.addEventListener("click", (e) => {
 window.onscroll = () => {
   searchBtn.classList.remove("fa-times");
   searchBar.classList.remove("active");
+  menuBar.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  loginForm.classList.remove("active");
 };
