180dd93295d05f26dc93506d3cfdd9958b9a6a65
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-04-23 07:48:49 +0300

    Trawel Website: Implemented review section

--- travel-website/js/main.js
+++ travel-website/js/main.js
@@ -45,3 +45,23 @@ window.onscroll = () => {
   navbar.classList.remove("active");
   loginForm.classList.remove("active");
 };
+
+const swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
