c03b21e1b95b3d77e9466be5c0f43542d3357f70
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-04-23 09:53:50 +0300

    Travel Website: finalize
    Implemented brand section
    Implemented footer section
    Fixed video links

--- travel-website/js/main.js
+++ travel-website/js/main.js
@@ -65,3 +65,26 @@ const swiper = new Swiper(".review-slider", {
     },
   },
 });
+
+const brandSwiper = new Swiper(".brand-slider", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
