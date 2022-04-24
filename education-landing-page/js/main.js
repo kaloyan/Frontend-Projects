6feef37394501aa4ca5a0c390bf30ef3e6405ec7
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-04-24 15:14:42 +0300

    Eduland: Implemented basic sidebar navigation

---
+++ education-landing-page/js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// navbar variables
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+[navOpenBtn, navCloseBtn, overlay].forEach((item) => {
+  item.addEventListener("click", (e) => {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+});
+
+// header sticky functionality
+const header = document.querySelector("[data=header]");
+
+window.addEventListener("scroll", (e) => {
+  window.scrollY >= 20
+    ? header.classList.add("active")
+    : header.classList.remove("active");
+});
